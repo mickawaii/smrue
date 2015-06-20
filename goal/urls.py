4bafa9f7d0c17fa70a1a5a7041af0d622e04283c
henriquekano <henriquekano@hotmail.com>
2015-06-20 13:45:36 -0300

    Adicionada edição de metas

--- goal/urls.py
+++ goal/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-from goal.views import IndexView, CreateView, DeleteView
+from goal.views import IndexView, CreateView, DeleteView, UpdateView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='list'),
     url(r'^create$', CreateView.as_view(), name='create'),
     url(r'^delete/(?P<pk>\d+)$', DeleteView.as_view(), name='delete'), 
+    url(r'^(?P<pk>\d+)$', UpdateView.as_view(), name='update'), 
 )
