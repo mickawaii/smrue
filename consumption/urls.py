dffa9aefb08cc2344e9d9e571f19c495fd93e2dc
henriquekano <henriquekano@hotmail.com>
2015-06-20 20:14:08 -0300

    Adicionada exportação de medidas em csv

--- consumption/urls.py
+++ consumption/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from consumption.views import graphicView
+from consumption.views import GraphicView, importCSV, exportCSV
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-  url(r'^$', graphicView, name='graphic'),
+  url(r'^$', GraphicView.as_view(), name='graphic'),
+  url(r'^importCSV$', importCSV, name='importCSV'),
+  url(r'^exportCSV$', exportCSV, name='exportCSV'),
+
 )
--- consumption/views.py
+++ consumption/views.py
@@ -3,12 +3,15 @@ from django.conf import settings
 from django.views.generic.base import View, TemplateView
 from consumption.forms import UploadFileForm
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
 from equipment.models import Equipment
 from consumption.models import Consumption
 
 from django.template import RequestContext
 import unicodecsv
+import csv
 
 # Create your views here.
 # class CoreListView(ListView, HasSystemMenuMixin):
@@ -19,12 +22,12 @@ import unicodecsv
 # 		return context
 
 
-# class GraphicView(TemplateView):
-# 	form_class = UploadFileForm
-# 	template_name = "consumption/graphic.html"
-# 	form = UploadFileForm()
+class GraphicView(TemplateView):
+	form_class = UploadFileForm
+	template_name = "consumption/graphic.html"
+	form = UploadFileForm()
 
-def graphicView(request):
+def importCSV(request):
 	if request.method == 'POST': # If the form has been submitted...
 		form = UploadFileForm(request.POST) # A form bound to the POST data
 		# if form.is_valid(): # All validation rules pass
@@ -34,6 +37,30 @@ def graphicView(request):
 			csv_reader = unicodecsv.DictReader(csv_imported, lineterminator = '\n', delimiter=';', encoding='latin-1')
 			for line in csv_reader:
 				Consumption.new(line['moment'], line['current'], line['voltage'], line['equipment_id']).save()
-			return render_to_response('consumption/graphic.html', {'form': form}, context_instance=RequestContext(request))
+			url = reverse('consumption:graphic')
+			return HttpResponseRedirect(url)
 		except Exception, e:
-			return render_to_response('consumption/graphic.html', {'csv_error': 'Erro na leitura do csv'}, context_instance=RequestContext(request))
+			url = reverse('consumption:graphic')
+			return HttpResponseRedirect(url)
+
+def exportCSV(request):
+	if request.method == 'GET':
+		response = HttpResponse(content_type='text/csv')
+		response['Content-Disposition'] = 'attachment; filename="report.csv"'
+		response.write(u'\ufeff'.encode('utf8'))
+		fieldnames = ['moment', 'current', 'voltage', 'equipment_id']
+		writer = csv.DictWriter(response, delimiter=";", fieldnames=fieldnames)
+
+		consumption_report = Consumption.objects.order_by('equipment_id', 'moment')
+		writer.writeheader()
+		for consumption in consumption_report:
+			writer.writerow(
+				{
+				'moment': consumption.moment, 
+				'current': consumption.current, 
+				'voltage': consumption.voltage, 
+				'equipment_id': consumption.equipment_id
+				}
+			)
+
+	return response
