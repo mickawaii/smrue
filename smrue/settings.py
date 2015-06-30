45a773deb13c57793ab5cf1eaac776177f7744ad
mickawaii <mickawaii@gmail.com>
2015-06-30 01:58:58 -0300

    resetting production database settings

--- smrue/settings.py
+++ smrue/settings.py
@@ -107,15 +107,4 @@ if socket.gethostname() == PRODUCTION_HOST_NAME:
 
 	DEBUG = TEMPLATE_DEBUG = False
 
-	DATABASES = {
-		'default': {
-			'ENGINE': 'django_postgrespool', 
-			'NAME': 'd312ostfbfevth',  
-			'USER': 'tkihlhiauawvvx',
-			'PASSWORD': 'SlfzPvh1rai6YSp6_lmckX3KfQ',
-			'HOST': 'ec2-54-227-249-165.compute-1.amazonaws.com',  
-			'PORT': '5432', 
-		}
-	}
-
-	DATABASES['default'] = dj_database_url.config()
+	DATABASES = {'default': dj_database_url.config(default='postgres://tkihlhiauawvvx:SlfzPvh1rai6YSp6_lmckX3KfQ@ec2-54-227-249-165.compute-1.amazonaws.com:5432/d312ostfbfevth')}
