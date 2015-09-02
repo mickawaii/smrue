69b92332fcc53b963b6e6bb553c1cf732d3e65f0
mickawaii <mickawaii@gmail.com>
2015-09-02 15:35:19 -0300

    added consumption static

--- smrue/settings.py
+++ smrue/settings.py
@@ -71,23 +71,23 @@ WSGI_APPLICATION = 'smrue.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-# DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.sqlite3',
-# 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-# 	}
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'df92jg90v64ufb',                      
-        'USER': 'kawmralupavplb',
-        'PASSWORD': 'LO9AMno_SHFuIM7IeSQGLajp2z',
-        'HOST': 'ec2-54-83-18-87.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'df92jg90v64ufb',                      
+#         'USER': 'kawmralupavplb',
+#         'PASSWORD': 'LO9AMno_SHFuIM7IeSQGLajp2z',
+#         'HOST': 'ec2-54-83-18-87.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
